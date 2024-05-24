bce92073ba41547619ead4d20a954acd2a6329ab
Walassi Silva <walassi.b@gmail.com>
2024-05-23 23:10:41 -0300

    style: fix mobile navbar color

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,10 +7,10 @@ import { FaBars, FaInstagram, FaTimes, FaWhatsapp } from "react-icons/fa";
 function NavLinks() {
   return (
     <>
-      <a className="px-2 py-1 rounded-md  hover:bg-white hover:text-red-500" href="#home">Home</a>
-      <a className="px-2 py-1 rounded-md  hover:bg-white hover:text-red-500" href="#about">Sobre</a>
-      <a className="px-2 py-1 rounded-md  hover:bg-white hover:text-red-500" href="#projects">Trabalhos</a>
-      <a className="px-2 py-1 rounded-md  hover:bg-white hover:text-red-500" href="#feedbacks">Feedbacks</a>
+      <a className="px-2 py-1 rounded-md  hover:bg-white hover:text-[#df1b1b]" href="#home">Home</a>
+      <a className="px-2 py-1 rounded-md  hover:bg-white hover:text-[#df1b1b]" href="#about">Sobre</a>
+      <a className="px-2 py-1 rounded-md  hover:bg-white hover:text-[#df1b1b]" href="#projects">Trabalhos</a>
+      <a className="px-2 py-1 rounded-md  hover:bg-white hover:text-[#df1b1b]" href="#feedbacks">Feedbacks</a>
     </>
   )
 }
@@ -22,18 +22,18 @@ function Contacts() {
   }
   return (
     <div>
-      <button className="flex items-center gap-1 px-2 py-1 text-end rounded-lg hover:bg-rose-400 hover:text-white "
+      <button className="flex items-center gap-1 px-2 py-1 text-end rounded-lg hover:bg-[#df1b1b] hover:text-white "
         onClick={toggleContacts}>
           < BiChevronDown className="rotate-90 md:rotate-0" />
         <p>Contatos</p>
         <ul className="relative">
           {isContactsOpen &&
             (
-              <div className="flex flex-col gap-3 right-28 text-xl -bottom-7 absolute text-white bg-red-400 rounded-md md:flex-row md:-bottom-[65px] md:right-[-10px] ">
-                <a className="p-2 rounded-full hover:bg-white hover:text-red-500" href="https://www.instagram.com/walassi_silva/" target="_blank"><FaInstagram /></a>
-                <a className="p-2 rounded-full hover:bg-white hover:text-red-500" href="https://www.youtube.com/" target="_blank"><AiOutlineYoutube /></a>
-                <a className="p-2 rounded-full hover:bg-white hover:text-red-500" href="https://wa.me/+553186482092" target="_blank"><FaWhatsapp /></a>
-                <a className="p-2 rounded-full hover:bg-white hover:text-red-500" href="#" target="_blank"><CgMail /></a>
+              <div className="flex flex-col gap-3 right-28 text-xl -bottom-7 absolute text-white bg-[#df1b1b] rounded-md md:flex-row md:-bottom-[65px] md:right-[-10px] ">
+                <a className="p-2 rounded-full hover:bg-white hover:text-[#df1b1b]" href="https://www.instagram.com/walassi_silva/" target="_blank"><FaInstagram /></a>
+                <a className="p-2 rounded-full hover:bg-white hover:text-[#df1b1b]" href="https://www.youtube.com/" target="_blank"><AiOutlineYoutube /></a>
+                <a className="p-2 rounded-full hover:bg-white hover:text-[#df1b1b]" href="https://wa.me/+553186482092" target="_blank"><FaWhatsapp /></a>
+                <a className="p-2 rounded-full hover:bg-white hover:text-[#df1b1b]" href="#" target="_blank"><CgMail /></a>
               </div>
             )}
         </ul>
@@ -64,7 +64,7 @@ export function Nav() {
         </div>
       </nav>
       {isOpen && (
-        <div className="flex flex-col items-end basis-full p-2 absolute right-1 top-12 ml-4 shadow border border-t-0 bg-white rounded-b-md">
+        <div className="dark:text-black flex flex-col items-end basis-full p-2 absolute right-1 top-14 ml-4 shadow border border-t-0 bg-white rounded-b-md">
 
           <div className="flex flex-col justify-center"
             onClick={() => setIsOpen(false)}>
